f349d7d64b084dbfd7b04c8ad37bbe71feb03a61
aaaleks07 <1138@htl.rennweg.at>
2023-04-06 13:31:04 +0200

    Programm creates a simple database with name and password (hashed) - Cannot store any other passwords

--- src/main/java/com/aleksa/jpasslocker/MainClass.java
+++ src/main/java/com/aleksa/jpasslocker/MainClass.java
@@ -5,20 +5,128 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.swing.*;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.Security;
 
 public class MainClass extends Application {
 
+    public static File file;
+    public static String passHashed;
+
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
+        startStage(stage);
+        //createDatabase(stage);
+    }
+
+    public static Scene createDatabase(Stage stage){
+        GridPane gridDatabase = new GridPane();
+        Scene sceneDatabase = new Scene(gridDatabase);
+        stage.setMinWidth(400);
+        stage.setMinHeight(350);
+
+        gridDatabase.setPadding(new Insets(-30,0,0,0));
+
+        gridDatabase.setAlignment(Pos.CENTER);
+
+        Label databaseTitle = new Label("Create Database");
+        databaseTitle.setFont(new Font(35));
+
+        GridPane.setHalignment(databaseTitle, HPos.CENTER);
+
+        Label databaseName = new Label("Name of Database");
+        TextField databaseNameInput = new TextField();
+        VBox dataBaseNameBox = new VBox(databaseName,databaseNameInput);
+
+        ProgressBar passStrengthBar = new ProgressBar(0);
+        Label entropyText = new Label("0");
+        StackPane passStrengthStackpane = new StackPane(passStrengthBar,entropyText);
+
+        Label password = new Label("Password");
+        PasswordField passwordInput = new PasswordField();
+        passwordInput.textProperty().addListener(observable -> {
+            double entropy = calculatePasswordEntropy(passwordInput.getText());
+            System.out.println(entropy);
+
+            if(entropy < 28){
+                passStrengthBar.setStyle("-fx-accent: darkred");
+                entropyText.setStyle("-fx-text-fill: black;");
+            } else if (entropy < 36) {
+                passStrengthBar.setStyle("-fx-accent: red");
+                entropyText.setStyle("-fx-text-fill: black;");
+            } else if (entropy < 60) {
+                passStrengthBar.setStyle("-fx-accent: yellow");
+                entropyText.setStyle("-fx-text-fill: black;");
+            } else if (entropy < 128) {
+                passStrengthBar.setStyle("-fx-accent: green");
+                entropyText.setStyle("-fx-text-fill: white;");
+            } else {
+                passStrengthBar.setStyle("-fx-accent: darkgreen");
+                entropyText.setStyle("-fx-text-fill: white;");
+            }
+            entropyText.setText(String.valueOf((double) Math.round(entropy*100) / 100));
+
+            passStrengthBar.setProgress(entropy/100 * 0.78125);
+        });
+        VBox passwordBox = new VBox(password,passwordInput);
+
+        passStrengthBar.prefWidthProperty().bind(gridDatabase.widthProperty().subtract(100));
+        passStrengthBar.setBorder(Border.EMPTY);
+
+        Button create = new Button("Create new Database");
+        HBox createBox = new HBox(create);
+        createBox.setPadding(new Insets(10,0,0,0));
+        create.prefWidthProperty().bind(passStrengthBar.prefWidthProperty());
+        create.setOnAction(actionEvent -> {
+            FileChooser filechooser = new FileChooser();
+            filechooser.setTitle("Create file");
+            filechooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JPassLocker Database", "*.jpldb"));
+
+            file = filechooser.showSaveDialog(stage);
+            try {
+                Files.createFile(Path.of(Path.of(file.getAbsolutePath()) + ".jpldb"));
+                BufferedWriter databaseFileBegin = Files.newBufferedWriter(
+                        Paths.get(file.getAbsolutePath() + ".jpldb"),
+                        StandardCharsets.UTF_8
+                );
+
+                databaseFileBegin.write(databaseNameInput.getText() + System.lineSeparator());
+                passHashed = String.valueOf(passwordInput.getText().hashCode() + System.lineSeparator());
+                databaseFileBegin.write(passwordInput.getText().hashCode() + System.lineSeparator());
+                databaseFileBegin.close();
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        gridDatabase.setVgap(10);
+        gridDatabase.add(databaseTitle,0,1);
+        gridDatabase.add(dataBaseNameBox,0,3);
+        gridDatabase.add(passwordBox,0,4);
+        gridDatabase.add(passStrengthStackpane,0,5);
+        gridDatabase.add(createBox, 0,6);
+
+        return sceneDatabase;
+    }
+
+
+    public static void startStage(Stage stage) {
         GridPane grid = new GridPane();
         Scene scene = new Scene(grid);
         stage.setMinWidth(400);
@@ -54,9 +162,8 @@ public class MainClass extends Application {
             }
         });
 
+        create.setOnAction(actionEvent -> {stage.setScene(createDatabase(stage));} );
         exit.setOnAction(actionEvent -> {System.exit(0);});
-
-
 
 
         grid.add(title,0,0);
@@ -70,9 +177,40 @@ public class MainClass extends Application {
 
         stage.setScene(scene);
         stage.show();
+
+
+        /**
+         * Create Database Scene
+         */
+
+        GridPane createDataBaseGrid = new GridPane();
+        Scene createDatabase = new Scene(createDataBaseGrid);
     }
+
+    public static double calculatePasswordEntropy(String password) {
+        String charSet = "";
+        if (password.matches(".*[a-z].*")) {
+            charSet += "abcdefghijklmnopqrstuvwxyz";
+        }
+        if (password.matches(".*[A-Z].*")) {
+            charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        if (password.matches(".*\\d.*")) {
+            charSet += "0123456789";
+        }
+        if (password.matches(".*[\\p{Punct}].*")) {
+            charSet += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+        }
+
+        int charSetSize = charSet.length();
+        int passwordLength = password.length();
+
+        return passwordLength * Math.log(charSetSize) / Math.log(2);
+    }
+
 
     public static void main(String[] args) {
         launch();
     }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.aleksa.jpasslocker {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.desktop;
 
     opens com.aleksa.jpasslocker to javafx.fxml;
     exports com.aleksa.jpasslocker;
