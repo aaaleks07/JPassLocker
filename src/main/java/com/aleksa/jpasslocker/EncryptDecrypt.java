4584aced4de492d9699b923c24bc7bcbc3433288
Sxmon17 <simonguglberger@gmail.com>
2023-04-11 14:54:52 +0200

    Optimize EncryptDecrypt class by refactoring common cipher initialization code

--- src/main/java/com/aleksa/jpasslocker/EncryptDecrypt.java
+++ src/main/java/com/aleksa/jpasslocker/EncryptDecrypt.java
@@ -5,52 +5,34 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
-/**
- * @author Nikolic Aleksa (aleksa.nikolic@htl.rennweg.at)
- */
 public class EncryptDecrypt {
     private static final String ALGORITHM = "AES";
     private static final int KEY_LENGTH = 16;
 
-    /**
-     * Encrypts a message using AES algorithm
-     * @param message
-     * @param password
-     * @return
-     * @throws Exception
-     */
     public static String encrypt(String message, String password) throws Exception {
         SecretKeySpec key = generateKey(password);
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, key);
+        Cipher cipher = initCipher(Cipher.ENCRYPT_MODE, key);
         byte[] encryptedBytes = cipher.doFinal(message.getBytes(StandardCharsets.UTF_8));
         return Base64.getEncoder().encodeToString(encryptedBytes);
     }
 
-    /**
-     * Decrypts a message using AES algorithm
-     * @param encryptedMessage
-     * @param password
-     * @return
-     * @throws Exception
-     */
     public static String decrypt(String encryptedMessage, String password) throws Exception {
         SecretKeySpec key = generateKey(password);
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, key);
-        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage));
+        Cipher cipher = initCipher(Cipher.DECRYPT_MODE, key);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedMessage.getBytes(StandardCharsets.UTF_8)));
         return new String(decryptedBytes, StandardCharsets.UTF_8);
     }
 
-    /**
-     * Generates a key for AES algorithm
-     * @param password
-     * @return
-     */
-    private static SecretKeySpec generateKey(String password){
+    private static SecretKeySpec generateKey(String password) {
         byte[] key = new byte[KEY_LENGTH];
         byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
         System.arraycopy(passwordBytes, 0, key, 0, Math.min(passwordBytes.length, key.length));
         return new SecretKeySpec(key, ALGORITHM);
+    }
+
+    private static Cipher initCipher(int mode, SecretKeySpec key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(mode, key);
+        return cipher;
     }
 }
